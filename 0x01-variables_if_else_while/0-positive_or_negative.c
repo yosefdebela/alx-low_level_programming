f164ee19b10f14282c66b761bcbf8426f8c520da
Yosefdebela <yosef.debela@ju.edu.et>
2022-07-01 13:26:28 -0700

    fdf

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,20 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdio.h>
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 /**
-*main -> assign ronadom number to variable
-*based a condition
-*Return : always 0
-*/
-int main (void)
+*main - Entry point
+*Return: Always 0 (success)
+**/
+int main(void)
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0)
-	printf("%d is positive\n", n);
-if (n == 0)
-	printf("%d is zero\n" , n);
-if (n < 0)
-{
-	printf("%d is negative\n", n);
+if (n > 0)	
+print("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else
+printf("%d is negative\n", n);
+return (0);
 }
-retutrn (0);
-}
-
