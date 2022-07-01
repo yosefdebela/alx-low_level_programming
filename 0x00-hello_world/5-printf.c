ee242884f09baf5e1e95dd3e3affea75df54b2fa
Yosefdebela <yosef.debela@ju.edu.et>
2022-06-30 22:59:43 -0700

    put

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
 /**
- * main - print 
+ * main - Entry point 
+ *
+ * return: Always 0 (success)
+ */
+ini main (void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,");
+return (0)
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,2 @@
 Preprocessor project
+Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
