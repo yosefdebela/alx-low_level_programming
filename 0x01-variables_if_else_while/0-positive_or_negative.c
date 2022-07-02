bd0af93fcc8d9b234709cbeac15b38361d23eb72
Yosefdebela <yosef.debela@ju.edu.et>
2022-07-02 13:03:35 -0700

    yosef

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
-* main
+* main - Entry point
 * Return: Always 0 (success)
 */
 int main(void)
@@ -11,13 +10,13 @@ int main(void)
 int n;
 int L;
 char str[] = "last digit of";
-srand(time(0);
-n = rand() - RAND_MAX /2:
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 L = n % 10;
 if (L > 5)
-printf("%s %d is %d and is 0\n" , str, n, L);
+printf("%s %d is %d and is greater than 5\n" , str, n, L);
 else if (L > 6)
-printf("%s %d is %d and is less than ^ and not 0\n" , str, n, L);
+printf("%s %d is %d and is less than 6 and not 0\n" , str, n, L);
 return (0);
 }
 		
