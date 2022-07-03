721d58d3965391915865056176479b5869e44c80
Yosefdebela <yosef.debela@ju.edu.et>
2022-07-03 11:55:08 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main -Entry point
  *
  * Return: Always 0 (success)
  */
-int main()
+int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
