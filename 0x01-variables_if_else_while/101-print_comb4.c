bbd6f0fa597bb0bf6caf6fabac68d24645609a10
Yosefdebela <yosef.debela@ju.edu.et>
2022-07-03 13:29:07 -0700

    100

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,15 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * * main - Entry point
- * * Return: Always 0 (Success)
- * */
+ *  main - Entry point
+ *  Return: Always 0 (Success)
+ * /
 int main(void)
 {
 	int d;
 	for (d = 0; d < 90; d++)
 	{
-	
 			putchar((d / 10) + '0');
 			putchar((d % 10) + '0');
 		}
