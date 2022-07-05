1bd745a3b3abc34db449ba77e75a18b27d1ed267
Yosefdebela <yosef.debela@ju.edu.et>
2022-07-05 12:33:23 -0700

    alphabetical checker

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/*
+ * _isalpha -checks for alphabetical letters
+ * @c: character to be checked
+ * return: return 0 or 1 based on the outcome value
+ */
+int _isalpha(int c)
+
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+
+}
