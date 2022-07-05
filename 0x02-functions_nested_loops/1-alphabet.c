892a08e04f45f861694243f024e5a0ba305b8e54
Yosefdebela <yosef.debela@ju.edu.et>
2022-07-05 10:10:03 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *prints the alphabet, in lowercase
+ *
+*/
+void print_alphabet(void)
+{
+		int y;
+		for (y = 'a' ; y <= 'z' ; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+
+}
