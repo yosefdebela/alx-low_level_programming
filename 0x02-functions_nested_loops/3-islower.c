c72d2a878e88c75d1217cdd43098979e484e8361
Yosefdebela <yosef.debela@ju.edu.et>
2022-07-05 14:54:34 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"                                             
-/**                                                           
-*_islower - Check desctiption                                 
-* @c: An input character                                      
-* Description: function uses _putchar function to print       
-* alphabet in lowercase 10 times                              
-* Return: 1 if is lowercase or 0 if is uppercase              
-*/ 
+/** 
+*_islower - Check desctiption
+* @c: An input character
+* Description: function uses _putchar function to print
+* alphabet in lowercase 10 times
+* Return: 1 if is lowercase or 0 if is uppercase
+*/
 int _islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
