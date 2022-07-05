bc7012babb4d317f905a71fb6aefd771df0b4822
Yosefdebela <yosef.debela@ju.edu.et>
2022-07-05 14:22:02 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,5 @@
 #include <unistd.h>
 #include "main.h"
-
 /**
  * times_table - Check the code
  * Description: Time table up to 9
@@ -10,33 +9,31 @@ void times_table(void)
 {
 int i, j, n;
 
-  for (i = 0; i <= 9; i++)
-	{
-    	for (j = 0; j <= 9; j++)
-	{
-      	n = i * j;
-
-      if ((n / 10) == 0)
-      {
-        if (j != 0)
-          _putchar(' ');
-        _putchar(n + '0');
-
-        if (j == 9)
-          continue;
-        _putchar(',');
-        _putchar(' ');
-      }
-      else
-      {
-        _putchar((n / 10) + '0');
-        _putchar((n % 10) + '0');
-        if (j == 9)
-          continue;
-        _putchar(',');
-        _putchar(' ');
-      }
-    }
-    _putchar('\n');
-  }
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+n = i * j;
+if ((n / 10) == 0)
+{
+if (j != 0)
+_putchar(' ');
+_putchar(n + '0');
+if (j == 9)
+continue;
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+if (j == 9)
+continue;
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 }
