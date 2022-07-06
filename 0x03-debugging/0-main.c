1f270de8e06bbfba91dd4475271eae79010a2338
Yosefdebela <yosef.debela@ju.edu.et>
2022-07-06 14:18:29 -0700

    multiple item

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - will check for positive_or_negative
+ * main Descripton -test function that prints if integer pos or negatiave
+ *  Return: 0 - up on success
+ */
+
+int main(void)
+
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
