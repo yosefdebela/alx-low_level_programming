070782c146344d44a9cea3bc2473581cd14fc616
Yosefdebela <yosef.debela@ju.edu.et>
2022-07-07 11:36:01 -0700

    print integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer n
+ *@n: integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
+	}
