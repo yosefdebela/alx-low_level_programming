9724d9b2734124a866b578a527ba5c31f1d1eb94
Yosefdebela <yosef.debela@ju.edu.et>
2022-07-11 10:41:53 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,35 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+
+/**
+ * main - paso
+ *
+ * Return: int 0;
+ */
+int main(void)
+{
+	char str[100];
+	int i = 0, randNum = 0, suma = 0;
+
+	srand (time(NULL));
+
+
+	for (i = 0; suma <= 2644; i++)
+	{
+		randNum = (rand() % 25) + 65;
+
+		str[i] = randNum;
+		suma = suma + randNum;
+	}
+
+	str[i++] = 2772 - suma;
+	str[i++] = '\0';
+
+	printf("%s\n", str);
+
+	return (0);
+}include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -21,4 +52,4 @@ int main(void)
 	}
 	printf("%c\n", (2772 - c));
 	return (0);
-}
+}}
