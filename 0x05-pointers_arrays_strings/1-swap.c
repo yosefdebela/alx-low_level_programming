8f5a1ad64c97ec1b32c1acd8d28aca25fc1191f8
Yosefdebela <yosef.debela@ju.edu.et>
2022-07-11 10:18:36 -0700

    swamp fixed

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
  * swap_int - swaps the values of two integers
- * a- integer to swap
- * b- integer to swap
+ * @a- integer to swap
+ * @b- integer to swap
  */
 
 void swap_int(int *a, int *b)
