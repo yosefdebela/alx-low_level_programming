8efe62f72730e575611d5049c70f01e83f556ada
yos <yosef.debela@ju.edu.com>
2022-09-29 22:28:30 +0300

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_dlistint - print all the elements of a dlistint_t list
- * @h: pointer to the start of the linked list
- *
- * Return: number of nodes
- */
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: pointer to the list.
+ * Return: number of nodes.
+ **/
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i;
+	const dlistint_t *node = h;
+	size_t cont = 0;
 
-	for (i = 0; h != NULL; i++)
+	while (node)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%i\n", node->n);
+		cont++;
+		node = node->next;
 	}
-	return (i);
+
+	return (cont);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,7 +1,8 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
-
-#include <stddef.h>
+#ifndef LISTS
+#define LISTS
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -14,9 +15,9 @@
  */
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
@@ -29,4 +30,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif /* _LISTS_H_ */
+#endif
