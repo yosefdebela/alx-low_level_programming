7e5f71a985010b9c444d9716b85bc8d2c3fa9687
yos <yosef.debela@ju.edu.et>
2023-11-30 23:20:02 +0300

    unix'

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO,"and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
